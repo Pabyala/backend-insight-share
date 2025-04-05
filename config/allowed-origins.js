91fd7e0488f1ed44f3350eed29935e863010bee3
Eleomar Fajutnao <109301348+Pabyala@users.noreply.github.com>
2025-04-05 11:40:43 +0800

    added: updated allowedOrigins

--- config/allowed-origins.js
+++ config/allowed-origins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-    // 'https://www.yoursite.com', 
-    'https://ins1ght-share.netlify.app',
+    'https://is-insight-share.vercel.app', 
+    // 'https://ins1ght-share.netlify.app',
     // 'http://localhost:8000',
     // 'http://localhost:3001',
     // 'http://localhost:3000'
